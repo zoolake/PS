784ea16917e7a46c3e77d07de81f1393a0cbbd2f
zoolake <57625026+zoolake@users.noreply.github.com>
2021-05-20 16:35:12 +0900

    Update BOJ_18310.py

--- BOJ_18310.py
+++ BOJ_18310.py
@@ -1,18 +1,6 @@
 import sys
-from collections import Counter
+
 input = sys.stdin.readline
 n = int(input())
-table = Counter(map(int,input().split()))
-
-min_dist = float('inf')
-result = float('inf')
-for antenna in table.keys():
-    dist = 0
-    for house, value in table.items():
-        dist += abs(antenna-house)
-
-    if min_dist > dist:
-        min_dist = dist
-        result = antenna   
-
-print(result)
+houses = sorted(list(map(int, input().split())))
+print(houses[(n-1)//2])
